3e5846797097798193469d18a6c69cc5bd676f9f
BHARATH SIMHA REDDY <bharathreddy8345@gmail.com>
2020-09-08 08:03:14 +0530

    Updates

---
+++ precache-manifest.2b6795b206d584245ff22ef57f029d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f0a0edc31f94d8adca2685e130d535",
+    "url": "/youtubevideo/index.html"
+  },
+  {
+    "revision": "fa990eb9c5118d3b3fdc",
+    "url": "/youtubevideo/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "17bd7c613a068a1f8918",
+    "url": "/youtubevideo/static/js/2.35399e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtubevideo/static/js/2.35399e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa990eb9c5118d3b3fdc",
+    "url": "/youtubevideo/static/js/main.9867526d.chunk.js"
+  },
+  {
+    "revision": "a442a0fdc5f0b8a7eac6",
+    "url": "/youtubevideo/static/js/runtime-main.bb0ced95.js"
+  }
+]);
